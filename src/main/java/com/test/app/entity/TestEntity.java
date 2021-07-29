b4ef59e8bca4a4da6355256977d72d07f06935ae
marianoazul <luismarianoazul@gmail.com>
2021-07-28 22:53:29 -0500

    Proyecto:test, Descripcion: Creacion del proyecto base

---
+++ src/main/java/com/test/app/entity/TestEntity.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.test.app.entity;
+
+/**
+ * @author Mariano Gomez Mtz
+ *
+ */
+public class TestEntity {
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+}
